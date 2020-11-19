30e490e4bfddca4085e2efff45368e30c06c03dd
賴慈馮 <t109598023@ntut.org.tw>
2020-11-19 17:14:33 +0800

    Initial commit

---
+++ src/main/java/edu/ntut/se1091/team1/pms/Application.java
@@ -0,0 +1,13 @@
+package edu.ntut.se1091.team1.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ntut/se1091/team1/pms/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ntut.se1091.team1.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
